04eeb325fb892a3d523b16c2c1dd171efbab316b
Breania Harvey <harvey.breania@gmail.com>
2021-10-10 16:13:40 -0500

    more prototype methods added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -37,3 +37,33 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test('gets players health value', () => {
+    const player = new Player('Dave');
+
+    // the expect.stringContaining() method is an expect method that can be used to make sure the string includes the players health
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+})
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+})
